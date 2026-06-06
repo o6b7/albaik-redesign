e1ecc3890a9eb68ead5395b7348d6f877e1657a5
o6b7 <alarkie2210@gmail.com>
2026-06-06 09:34:50 +0300

    profile page skeleton

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,49 @@
-import { Text } from 'react-native';
+import { useAuthStore } from '@/store/auth-store';
+import { ChevronRight, CreditCard, MapPin, User } from 'lucide-react-native';
+import { Image, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ProfileScreen() {
+  const user = useAuthStore((state) => state.user);
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl">Profile Screen</Text>
+    <SafeAreaView>
+      <Text className="text-xl font-bold text-center my-8">My Account</Text>
+      <View className='w-full px-4'>
+        <View className='flex-row items-center p-4 rounded-lg'>
+          <Image source={require('@/assets/images/profileImage.jpg')} className='w-24 h-24 rounded-full' />
+          <Text className='text-2xl font-bold ml-4'>{user?.fullName}</Text>
+        </View>
+        <View className='px-3 gap-8 mt-5'>
+          <View className='flex-row items-center border-b-[1px] border-[#cecece] pb-4 w-full'>
+            <View className='p-2 border-2 border-[#9e9e9e] rounded-full'>
+              <User size={35} color={'#9e9e9e'} />
+            </View>
+            <View className='justify-between flex-row w-[85%] items-center'>
+              <Text className='ml-5 text-xl font-bold text-[#494949]'>Account Informations</Text>
+              <ChevronRight size={20} color={'#9e9e9e'} />
+            </View>
+          </View>
+          <View className='flex-row items-center border-b-[1px] border-[#cecece] pb-4'>
+            <View className='p-2 border-2 border-[#9e9e9e] rounded-full'>
+              <CreditCard size={35} color={'#9e9e9e'} />
+            </View>
+            <View className='justify-between flex-row w-[85%] items-center'>
+              <Text className='ml-5 text-xl font-bold text-[#494949]'>Payment Methods</Text>
+              <ChevronRight size={20} color={'#9e9e9e'} />
+            </View>
+          </View>
+          <View className='flex-row  items-center border-b-[1px] border-[#cecece] pb-4'>
+            <View className='p-2 border-2 border-[#9e9e9e] rounded-full'>
+              <MapPin size={35} color={'#9e9e9e'} />
+            </View>
+            <View className='justify-between flex-row w-[85%] items-center'>
+              <Text className='ml-5 text-xl font-bold text-[#494949]'>Delivery Location</Text>
+              <ChevronRight size={20} color={'#9e9e9e'} />
+            </View>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
